45e647bf9b6cae47734a9ae5e1da62a78f09b451
TakayukiMorijiri <takayuki.m1206@gmail.com>
2016-06-01 12:34:30 +0900

    Text機能の練習

--- sampleTextField/ViewController.swift
+++ sampleTextField/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func tapReturn(sender: UITextField) {
+        
+        print(sender.text)
+        //TODO:画面にラベルを用意してtestという文字が含まれていたら「ｔｅｓｔです」とラベルに表示、それ以外は入力された文字を入力
+        
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
